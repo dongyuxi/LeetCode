a848b3e956f9428f11af74a190765e99596904e7
dongyuxi <orange2010road@gmail.com>
2015-04-28 22:48:42 +0800

    Add leetcode problem 097.

--- LeetCode/src/com/leetcode/problem097/interleavingstring/Solution.java
+++ LeetCode/src/com/leetcode/problem097/interleavingstring/Solution.java
@@ -27,23 +27,20 @@ public class Solution {
         if (s3.length() != s1.length() + s2.length()) {
             return false;
         }
-        return isInterleave(s1, 0, s2, 0, s3, 0);
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        for (int i = 1; i <= s2.length(); i++) {
+            dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                        || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[s1.length()][s2.length()];
     }
-
-    private boolean isInterleave(String s1, int index1, String s2, int index2,
-            String s3, int index3) {
-        if (index1 == s1.length() && index2 == s2.length() && index3 == s3.length()) {
-            return true;
-        }
-
-        boolean flag = false;
-        if (index1 != s1.length() && s1.charAt(index1) == s3.charAt(index3)) {
-            flag = flag || isInterleave(s1, index1 + 1, s2, index2, s3, index3 + 1);
-        }
-        if (index2 != s2.length() && s2.charAt(index2) == s3.charAt(index3)) {
-            flag = flag || isInterleave(s1, index1, s2, index2 + 1, s3, index3 + 1);
-        }
-        return flag;
-    }
-
 }
