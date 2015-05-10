97d6465c7b5b8a1ffe2f455d69808c5dba52e524
dongyuxi <orange2010road@gmail.com>
2015-05-10 17:08:00 +0800

    Add leetcode problem 200.

---
+++ LeetCode/tst/com/leetcode/problem200/numberofislands/SolutionTest.java
@@ -0,0 +1,58 @@
+package com.leetcode.problem200.numberofislands;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ * 
+ * Example 1:
+
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * Answer: 1
+
+ * Example 2:
+
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * Answer: 3
+ * 
+ * @author dongyuxi
+ *
+ */
+public class SolutionTest {
+    public Solution solution = null;
+
+    @Before
+    public void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test() {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        assertEquals(1, solution.numIslands(grid));
+
+        grid = new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        assertEquals(3, solution.numIslands(grid));
+    }
+}
